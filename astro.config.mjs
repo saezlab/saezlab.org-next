c19792c3885b2f7d37323a8b8537fde925768b85
deeenes <turei.denes@gmail.com>
2024-01-29 14:36:42 +0100

    Astro config: define `site` and `base` for hosting under subpath

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://saezlab.github.io',
+  base: '/saezlab.org-next',
   integrations: [svelte()]
 });
